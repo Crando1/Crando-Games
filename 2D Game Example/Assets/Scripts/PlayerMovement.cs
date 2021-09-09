9bc61e1cc462d5f8ae736991700b3de93e7962f1
Crando1 <connor.randolph@gmail.com>
2021-09-09 13:09:51 -0400

    Update to items

--- 2D Game Example/Assets/Scripts/PlayerMovement.cs
+++ 2D Game Example/Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private BoxCollider2D coll;
     private Animator anim;
     private SpriteRenderer sprite;
+
+    [SerializeField] private LayerMask jumpableGround;
+
 
     private float dirx =0f;
    [SerializeField] private float moveSpeed = 7f;
@@ -21,6 +25,7 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
+        coll = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -30,7 +35,7 @@ public class PlayerMovement : MonoBehaviour
         rb.velocity = new Vector2(dirx * moveSpeed, rb.velocity.y);
 
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
@@ -71,7 +76,10 @@ public class PlayerMovement : MonoBehaviour
 
         anim.SetInteger("state", (int)state);
     }
-
+     private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+    }
 
 
 }
