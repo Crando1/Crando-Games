104f734b5c979914b502a6afe73f390f65550723
Crando1 <connor.randolph@gmail.com>
2021-10-30 10:34:13 -0400

    Break rock when hits wall
    Worked on putting in animation when rock hits the wall. Use this as a base if you can't get sprit to work

--- Demo_Elementals/Elemental Demo/Assets/Scripts/RockThrow.cs
+++ Demo_Elementals/Elemental Demo/Assets/Scripts/RockThrow.cs
@@ -8,6 +8,8 @@ public class RockThrow : MonoBehaviour
     [SerializeField] private Rigidbody2D rb2d;
     public Vector2 castDirection;
     [SerializeField] private float castForce = 100f;
+    [SerializeField] private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +19,7 @@ public class RockThrow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
 
@@ -27,4 +29,38 @@ public class RockThrow : MonoBehaviour
         rb2d.AddForce(castDirection * castForce , ForceMode2D.Impulse);
 
     }
+
+
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        {
+            if (collision.gameObject.CompareTag("Earth"))
+            {
+                anim.SetTrigger("isBroken");
+               
+
+
+            }
+        }
+
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        
+    }
+
+    private void DestroyRock()
+    {
+
+        Destroy(gameObject);
+    }
 }
+    
+
+
+
+
+
+
+
